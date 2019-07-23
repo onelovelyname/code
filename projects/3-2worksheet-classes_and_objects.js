63365678122b856e43ce3ab03bc424ed20edbdb5
Orjwan Al-Rajaby <orjwan98@hotmail.com>
2019-07-23 15:51:00 +0300

    Made worksheet for classes and objects

---
+++ projects/3-2worksheet-classes_and_objects.js
@@ -0,0 +1,53 @@
+// Classes project - in groups of 2, create a class for a type you imagine: Animal, Person, Country...anything!
+// Must have 3 methods and 5 properties
+// Start with using the functional way of creating an object. If you have time, write it again using the class keyword
+
+//======================Object literals==========================//
+
+//write a literal object that contains at least 3 different properties and one method.
+
+//the object can be anything you want: car, animal, country, person
+
+//add a new property to your object using bracket-notation.
+
+//change one of your object's properties using dot-notation.
+
+//add a new property to your object using bracket-notation and a variable.
+
+//The method can do anything you want. Call it.
+
+//BONUS: write another method that -when called- logs one of the properties to the console using the 'this' keyword.
+
+// write your code below this line
+
+//===============================CLASSES====================================//
+
+//write your previous object using a class. Make 3 different instances of your class.
+
+//Also, add and change properties on your object using:
+
+//1. dot notation.
+//2. bracket notation.
+//3 variable with bracket notation.
+
+//BONUS: write another method that -when called- logs one of the properties to the console using the 'this' keyword.
+
+//write your code here
+
+//===============================functional classes=========================//
+
+//write a function that returns an object (animal, car, person) each time it's called with 3 properties and two methods.
+
+//call your function, and log the returned object to the console.
+
+//write your code here.
+
+//====
+
+//write a class-based function that has three different properties and two methods which returns an object (animal, car, person) each time an instance is made.
+
+//log each one of your instances to the console.
+
+//BONUS: define another method on your class that -when called- logs one of the properties to the console using the 'this' keyword.
+
+//write your code here.
