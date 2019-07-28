62f03308571750b77b133dacc9c0da39bfd3b6b7
Orjwan Al-Rajaby <orjwan98@hotmail.com>
2019-07-28 22:16:21 +0300

    made worksheet for dom manipualtion lecture

--- projects/script.js
+++ projects/script.js
@@ -1,1 +1,12 @@
+//connect this script file to index.html
 
+//using JS:
+
+//1. color ALL TEXT in index.html with any color you like.
+//hint: children inherit styles from parent elements.
+
+//2. Allow the user to add new items to the 'hobbies' section using an input and a button.
+//hint: use an event-listener on the button.
+
+//BONUS:
+//3. Allow the user to enter a color and change the body's background color to be the entered color.
