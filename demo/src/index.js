d45b770850bf39b9b4bb33c9fb1545278bf51d01
Melanie Gin <mg@carefreebreaks.com>
2019-08-01 14:17:34 +0300

    Update with code from the React lecture on day4-3

--- demo/src/index.js
+++ demo/src/index.js
@@ -3,19 +3,88 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          Hello everyone!
-        </h1>
-      </header>
-    </div>
-  );
+// function Component
+// function App() {
+//   return (
+//     <div className="App">
+//     	<div>
+// 	    	<input type="text" placeholder="What's your favorite color?" id="color-input"/>
+// 	    	<button id="submit-button">Submit</button>
+//     	</div>
+//     	<div>
+// 	    	<img src="./sunflower.jpeg" />
+// 	    	<img src="./sunflower.jpeg" />
+// 	    	<img src="./sunflower.jpeg" />
+//     	</div>
+//     </div>
+//   );
+// }
+
+// class Component
+class App1 extends React.Component {
+	render() {
+	  return (
+	    <div className="App">
+	    	<h1>App1</h1>
+	    	<SearchBar />
+	    	<div>
+	    		<Photo nature="sunflower" />
+	    		<Photo nature="rose" />
+	    		<Photo nature="olive" />
+	    	</div>
+	    </div>
+	  );
+	}
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Photo extends React.Component {
+	state = {
+		photoSrc: this.props.nature + ".jpeg"
+	}
+	render() {
+		return (
+		// JSX
+			<div>
+		    	<img src={this.state.photoSrc} />
+		    	<p>{this.props.nature}</p>	    			
+			</div>
+			);
+	}
+}
+
+class SearchBar extends React.Component {
+	state = {
+		inputValue: ""
+	}
+	
+	render() {
+		return (
+	    	<div>
+		    	<input type="text" placeholder="What's your favorite color?" id="color-input"/>
+		    	<button id="submit-button">Submit</button>
+	    	</div>
+			);
+	}
+}
+
+// // function Component
+// function Welcome1(props) {
+// 	console.log(props);
+// 	return <h2>Welcome to our website {props.name}! We're in {props.area}</h2>
+// }
+
+// // class Component
+// class Welcome extends React.Component {
+// 	render() {
+// 		console.log("Welcome props: ", this.props);
+// 		return <h1>Hello {this.props.name}</h1>;
+// 	}
+// }
+
+ReactDOM.render(
+	<App1 />, 
+	document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
