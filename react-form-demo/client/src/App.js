9792cdc68347eab172d84986eedae0b73694c6e6
Orjwan Al-Rajaby <orjwan98@hotmail.com>
2019-08-01 09:44:13 +0300

    import Form and Gallery. Set up event handlers

--- react-form-demo/client/src/App.js
+++ react-form-demo/client/src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Form from './components/Form';
+import Gallery from './components/Gallery';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      image: '',
+      currentImage: ''
+    };
+
+    this.addImageHandler = this.addImageHandler.bind(this);
+    this.submitImage = this.submitImage.bind(this);
+  }
+
+  addImageHandler(event) {
+    var value = event.target.value;
+    this.setState({ currentImage: value });
+  }
+
+  submitImage(event) {
+    event.preventDefault();
+    this.setState({ image: this.state.currentImage });
+  }
+
+  render() {
+    var image = this.state.image;
+    var currentImage = this.state.currentImage;
+
+    return (
+      <div className="App">
+        <Form
+          addImageHandler={this.addImageHandler}
+          submitImage={this.submitImage}
+          currentImage={currentImage}
+        />
+
+        <Gallery image={image} />
+      </div>
+    );
+  }
 }
 
 export default App;
